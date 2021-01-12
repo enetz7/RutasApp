d76769bcf64939e9f3966489c7c3bf7ca31cdb10
enetz7 <enetz7@outlook.com>
2021-01-12 16:49:44 +0100

    Feat:styles

--- app/views/panel/ranking/carta.tsx
+++ app/views/panel/ranking/carta.tsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { Row } from "react-bootstrap";
 import { View, Text, StyleSheet, Image, ViewStyle } from "react-native";
 import { Usuario } from "../../interface/usuario";
-import { LinearGradient } from 'expo-linear-gradient';
+import { LinearGradient } from "expo-linear-gradient";
 export interface CartaProps {
   usuario: Usuario;
   indice: number;
@@ -13,35 +13,34 @@ export interface CartaProps {
 export function Carta(props: CartaProps) {
   const image = "./avatares/avatarPorDefecto.jpg";
   let RankingStyle;
-  let gradientColor=[];
+  let gradientColor = [];
   if (props.indice == 0) {
     RankingStyle = styles.vistaPrimero;
-    gradientColor=["#B78628","#C69320","#DBA514","#EEB609","#FCC201"];
+    gradientColor = ["#fceabb", "#f8b500"];
   } else if (props.indice == 1) {
     RankingStyle = styles.vistaSegundo;
-    gradientColor=["#D7D7D8","#C7C9CB","#AEB2B8","#959BA3","#848B98"];
+    gradientColor = ["#bdc3c7", "#2c3e50"];
   } else if (props.indice == 2) {
     RankingStyle = styles.vistaTercero;
-    gradientColor=["#804A00","#895E1A","#9C7A3C","#B08D57"];
+    gradientColor = ["#b29f94", "#603813"];
   } else {
     RankingStyle = styles.vista;
-    gradientColor=["white","white"];
+    gradientColor = ["white", "white"];
   }
   return (
     <LinearGradient
-       // Button Linear Gradient
-       colors={gradientColor}
-       style={RankingStyle}
-       >
-
-      
+      start={{ x: 0, y: 1 }}
+      end={{ x: 1, y: 0 }}
+      // Button Linear Gradient
+      colors={gradientColor}
+      style={RankingStyle}
+    >
       <Image
         style={{ width: 50, height: 50, borderRadius: 25 }}
         source={require(image)}
       ></Image>
       <Text>{props.usuario.nombre}</Text>
       <Text style={styles.textoPuntuacion}>{props.puntuacion}</Text>
-
     </LinearGradient>
   );
 }
